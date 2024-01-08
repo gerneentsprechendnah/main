e84ad5c3cf677d45b168360f55c75bbbe65add43
Stephan <stephan@ritthaler.me>
2024-01-08 08:51:28 +0100

    angepasst

--- desktop.nix
+++ desktop.nix
@@ -2,6 +2,6 @@
   home-manager.users.stephan = {
     /* The home.stateVersion option does not have a default and must be set */
     home.stateVersion = "23.11";
-  home.packages = [ pkgs.jdk8 ];
+  home.packages = [ pkgs.gedit ];
 };
 }
--- home.nix
+++ home.nix
@@ -1,11 +1,13 @@
 { config, pkgs, ... }:
 let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  home-manager = (builtins.fetchTarball {
+  url= "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  sha256 = "1a19f91vkpdi95l39fplrl3kfxmknkmbys4qi3fxibj751slm0y8";});
 in
 {
   imports = [
     (import "${home-manager}/nixos")
-     ( import "/home/stephan/flake/desktop.nix")
+    ( import "/home/stephan/flake/desktop.nix")
   ];
  
  
