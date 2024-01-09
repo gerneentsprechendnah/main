e0706821c58ce03eef7db3f533384ae53a10d54a
Stephan <stephan@ritthaler.me>
2024-01-09 06:12:00 +0100

    test

---
+++ programs.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+home-manager.users.stephan = {
+    /* The home.stateVersion option does not have a default and must be set */
+    home.stateVersion = "23.11";
+    home.packages = [ 
+      pkgs.thunderbird 
+      pkgs.chromium 
+      pkgs.typora 
+      pkgs.bitwarden
+    ];
+  };
+}
