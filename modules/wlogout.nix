90e283888df45b1c8be02fbac1fb8833e19f43d6
Stephan <stephan@ritthaler.me>
2024-01-14 22:44:10 +0100

    logout

--- modules/wlogout.nix
+++ modules/wlogout.nix
@@ -1,9 +1,6 @@
 { config, ...}: {
 
-users.users.stephan.isNormalUser = true;
 home-manager.users.stephan = {
-/* The home.stateVersion option does not have a default and must be set */
-home.stateVersion = "23.11";
 
 
  programs.wlogout = {
