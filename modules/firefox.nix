02ac85b4f9561aca50e8df80f5010cdbfbb06b54
gerneentsprechendnah <155282186+gerneentsprechendnah@users.noreply.github.com>
2024-03-13 14:50:18 +0100

    Update firefox.nix

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -31,7 +31,33 @@ params = [
       url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
     }
 
- {    name = "wikipedia";    tags = [ "wiki" ];    keyword = "wiki";    url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";  }  {    name = "kernel.org";    url = "https://www.kernel.org";  }  {    name = "Nix sites";    toolbar = true;    bookmarks = [      {        name = "homepage";        url = "https://nixos.org/";      }      {        name = "wiki";        tags = [ "wiki" "nix" ];        url = "https://nixos.wiki/";      }    ];  }
+    {
+      name = "wikipedia";    
+      tags = [ "wiki" ];    
+      keyword = "wiki";    
+      url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";   
+     }  
+
+    {    
+      name = "kernel.org";    
+      url = "https://www.kernel.org";   
+     }  
+
+    {    
+    name = "Nix sites";    
+    toolbar = true;    
+    bookmarks = [      
+                  {        
+                    name = "homepage";        
+                    url = "https://nixos.org/";     
+                    }      
+                  {  
+                    name = "wiki";        
+                    tags = [ "wiki" "nix" ];        
+                    url = "https://nixos.wiki/";       
+                  }    
+                ];  
+      }
 
     {
       name = "";
